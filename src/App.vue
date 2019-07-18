a3b344d9223e2ad4cc0af75605f6ece63b8dc96d
tuffstudio <chris@tuff-studio.com>
2019-07-18 12:09:13 +0100

    content responsive

--- src/App.vue
+++ src/App.vue
@@ -13,76 +13,95 @@
       </div>
     </header>
 
-    <main class="px-5 py-6">
+    <main class="px-5 py-6 md:px-10 max-w-6xl xl:mx-auto">
 
       <div class="hidden sm:flex justify-between items-center">
         <h1 class="text-2xl font-semibold" >Welcome to LANDR</h1>
         <a class="px-6 py-3 inline-block bg-yellow-500 text-xs leading-none font-semibold text-white rounded-sm" href="#">See Plans</a>
       </div>
-
-      <section class="mt-4 flex shrink-0">
-        <div class="relative p-5 w-full">
+<!-- banner -->
+      <section class="mt-4 md:mt-8 flex shrink-0">
+        <div class="relative p-5 md:px-12 md:py-24 w-full">
           <img class="absolute inset-0 h-full w-full rounded object-cover" src="./assets/img/banner-collab.jpg" alt="Guy in the music studio">
-          <div class="p-4 w-9/12 relative">
-            <h2 class="text-3xl leading-tight text-white font-semibold tracking-wide">Share tracks & ask for opinions.</h2>
+          <div class="p-4 md:p-6 w-9/12 max-w-lg relative">
+            <h2 class="text-3xl md:text-5xl leading-tight md:leading-none text-white font-semibold tracking-wide">Share tracks & ask for opinions.</h2>
             <p class="mt-3 text-white">Take notes for yourself, or ask for feedback after you've mastered.</p>
             <a class="mt-5 inline-block px-8 py-3 bg-gray-100 text-sm text-gray-700 font-semibold rounded-sm" href="#">Get feedback</a>
           </div>
         </div>
       </section>
 
-      <section class="block sm:flex sm:flex-wrap sm:-mx-2">
-        <div v-for="(stat, i) in stats" class="mt-4 card text-center sm:w-1/2 sm:px-2">
-          <UserStats :stat="stat"/>
+      <div class="block lg:flex">
+	<!-- stats -->
+	      <section class="block sm:flex sm:flex-wrap sm:-mx-2 lg:w-1/2">
+	        <div v-for="(stat, i) in stats" class="card mt-4 text-center sm:w-1/2 sm:px-2">
+	          <UserStats :stat="stat"/>
+	        </div>
+	      </section>
+	<!-- tracks -->
+	      <section class="mt-4 bg-white rounded lg:w-1/2 lg:ml-4">
+	        <div class="p-6 inline-block w-full">
+	          <h2 class="text-sm text-gray-500">Recent work</h2>
+	            <div class="mt-8">
+	                <div v-for="(track, i) in tracks" :class="{'border-t' : i === 0 }" class="border-b">
+	                  <MasteredTrack :track="track"/>
+	                </div>
+	            </div>
+	            <a class="mt-1 inline-block text-xs float-right font-semibold" href="#">See all files</a>
+	        </div>
+	      </section>
+      </div>
+      <section class="lg:flex lg:-mx-2">
+  <!-- blog -->
+        <div class="mt-4 lg:w-1/2 lg:pl-2 lg:pr-4 ">
+          <section class="card h-full bg-white rounded hover:shadow-lg">
+            <a href="#" class="p-6 inline-block md:flex justify-between">
+              <div>
+                <h2 class="text-sm text-gray-500">The LANDR Blog</h2>
+                <p class="mt-4 text-lg font-semibold">Get the best tools, tips and inspiration you need to grow your music everyday.</p>
+              </div>
+              <img class="hidden md:block h-24 w-32 p-2" src="./assets/img/ill-empty_state-blog.svg" alt="">
+            </a>
+          </section>
         </div>
-      </section>
-
-
-      <section class="mt-4 bg-white rounded">
-        <div class="p-6 inline-block w-full">
-          <h2 class="text-sm text-gray-500">Recent work</h2>
-            <div class="mt-8">
-                <div v-for="(track, i) in tracks" :class="{'border-t' : i === 0 }" class="border-b">
-                  <MasteredTrack :track="track"/>
-                </div>
-            </div>
-            <a class="mt-1 inline-block text-xs float-right font-semibold" href="#">See all files</a>
-        </div>
-      </section>
-
-      <section class="mt-4 card bg-white rounded hover:shadow-lg">
-        <a href="#" class="p-6 inline-block">
-          <h2 class="text-sm text-gray-500">The LANDR Blog</h2>
-          <p class="mt-4 text-lg font-semibold">Get the best tools, tips and inspiration you need to grow your music everyday.</p>
-        </a>
-      </section>
-
-       <section class="mt-4 relative card rounded hover:shadow-lg">
-        <div class="p-6 relative z-10 sm:w-7/12">
-          <a href="#" class="inline-block">
-            <h2 class="text-sm text-gray-500">LANDR Samples</h2>
-            <p class="mt-4 text-lg font-semibold">Stay inspired with free curated sample packs from top artists.</p>
-          </a>
-        </div>
-        <img class="absolute inset-0 w-full h-full object-cover" src="./assets/img/samples-background@2x.jpg" alt="Hand playing synth">
-      </section>
-
-      <section class="mt-4 p-6 bg-white rounded">
-        <h2 class="text-2xl leading-tight font-semibold tracking-wide">Stay in the loop with the LANDR newsletter</h2>
-        <p class="mt-4 text-sm text-gray-600">Sing up to get the best music tips, tools and tutorials direct to your inbox.</p>
-        
-        <form class="w-full mt-12">
-          <div class="flex items-center border">
-            <input class="py-1 px-4 appearance-none bg-transparent w-full text-gray-700 leading-tight focus:outline-none" type="email" placeholder="Email" aria-label="Email">
-            <button class="py-3 px-5 flex-shrink-0 border-l text-sm font-semibold text-teal-400" type="button">
-              Subscribe
-            </button>
+  <!-- samples -->
+        <div class="mt-4 lg:w-1/2 lg:pl-0 lg:pr-2 ">
+         <section class="card relative h-full rounded hover:shadow-lg">
+          <div class="p-6 relative z-10 sm:w-7/12">
+            <a href="#" class="inline-block">
+              <h2 class="text-sm text-gray-500">LANDR Samples</h2>
+              <p class="mt-4 text-lg font-semibold">Stay inspired with free curated sample packs from top artists.</p>
+            </a>
           </div>
-        </form>     
+          <img class="absolute inset-0 w-full h-full object-cover rounded" src="./assets/img/samples-background@2x.jpg" alt="Hand playing synth">
+        </section>
+        </div>
+      </section>
+<!-- newsletter -->
+      <section class="mt-4 p-6 bg-white rounded">
+
+        <div class="md:flex md:items-center md:-mx-2">
+          <div class="w-full md:w-1/2 md:px-2">
+            <h2 class="text-2xl leading-tight font-semibold tracking-wide">Stay in the loop with the LANDR newsletter</h2>
+            <p class="mt-4 text-sm text-gray-600">Sing up to get the best music tips, tools and tutorials direct to your inbox.</p>
+          </div>
+          
+          <div class="w-full md:w-1/2 md:px-2">
+            <form class="w-full mt-12 md:mt-0">
+              <div class="flex items-center border">
+                <input class="py-1 px-4 appearance-none bg-transparent w-full text-gray-700 leading-tight focus:outline-none" type="email" placeholder="Email" aria-label="Email">
+                <button class="py-3 px-5 flex-shrink-0 border-l text-sm font-semibold text-teal-400" type="button">
+                  Subscribe
+                </button>
+              </div>
+            </form>   
+          </div> 
+        </div> 
+
       </section>
 
     </main>
-    <footer class="px-4 py-2 border-t sm:flex sm:justify-between">
+    <footer class="px-4 py-2 md:px-10 border-t sm:flex sm:justify-between">
       <div>
         <a class="text-sm underline hover:no-underline" href="#">Privacy Policy</a>
         <span class="mx-1">-</span>
